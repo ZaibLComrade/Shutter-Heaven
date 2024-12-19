223024813a7a177b1cdd4f1bad9fed5b4573eb60
ZaibLComrade <kmjahanzaib@proton.me>
2024-12-20 04:01:41 +0600

    Set up basic auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
+import AuthProvider from "./provider/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router}/>
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
 	</StrictMode>
 );
---
+++ src/provider/AuthProvider.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, ReactNode } from "react";
+import { AuthContext } from ".";
+import { auth } from "@/config/firebase.config";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	onAuthStateChanged,
+	signOut,
+	User,
+} from "firebase/auth";
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+	const [user, setUser] = useState<User | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (userCredential) => {
+			setUser(userCredential);
+			setLoading(false);
+		});
+		return () => unsubscribe();
+	}, []);
+
+	const registerUser = (email: string, password: string) => {
+		setLoading(false);
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+
+	const loginUser = (email: string, password: string) => {
+		setLoading(false);
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+
+	const logoutUser = () => {
+		setLoading(false);
+		return signOut(auth);
+	};
+
+	const contextValues = {
+		user,
+		loading,
+		setLoading,
+		registerUser,
+		loginUser,
+		logoutUser,
+	};
+	return (
+		<AuthContext.Provider value={contextValues}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export default AuthProvider;
